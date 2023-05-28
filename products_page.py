88e5293f9454814f637b35635571f01e99ab606f
Tsihar <tsitavets.ihar@gmail.com>
2023-05-28 23:15:30 +0300

    5th_task_correction

--- products_page.py
+++ products_page.py
@@ -6,17 +6,17 @@ class Products_page():
         self.driver = driver
 
     def login_verification(self):
-        page_products = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='title']")))
+        page_products = WebDriverWait(self.driver, 6).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='title']")))
         page_title_text = page_products.text
         assert page_title_text == "Products"
 
     def logout(self):
-        burger_menu_button = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='react-burger-menu-btn']")))
+        burger_menu_button = WebDriverWait(self.driver, 6).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='react-burger-menu-btn']")))
         burger_menu_button.click()
 
-        logout_button = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='logout_sidebar_link']")))
+        logout_button = WebDriverWait(self.driver, 6).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='logout_sidebar_link']")))
         logout_button.click()
 
-        login_page = WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='login_logo']")))
+        login_page = WebDriverWait(self.driver, 6).until(EC.element_to_be_clickable((By.XPATH, "//div[@class='login_logo']")))
         login_page_title = login_page.text
         assert login_page_title == "Swag Labs"
